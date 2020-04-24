fa4d433d500f82e7ad595996415ac620b6a7c396
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-04-24 17:05:19 +0200

    parte 5

--- Navigation.js
+++ Navigation.js
@@ -1,25 +1,34 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './screens/HomeScreen';
-import PostDetail from './screens/PostDetailScreen';
-import Profile from './screens/ProfileScreen';
-import SavedPost from './screens/SavedPostScreen'
-
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Home from "./screens/HomeScreen";
+import PostDetail from "./screens/PostDetailScreen";
+import Profile from "./screens/ProfileScreen";
+import SavedPost from "./screens/SavedPostScreen";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-function Navigation() {
+function StackNavigation() {
   return (
-    <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="PostDetail" component={PostDetail} />
         <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="SavedPost" component={SavedPost} />
       </Stack.Navigator>
+  );
+}
+
+function DrawerNavigation() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={StackNavigation} />
+        <Drawer.Screen name="SavedPost" component={SavedPost} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
 
-export default Navigation;
+export default DrawerNavigation;
