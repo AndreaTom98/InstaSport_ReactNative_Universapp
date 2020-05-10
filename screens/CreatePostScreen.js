fcef1a133ee5bc1438a9173f3e10ee4e88320a61
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-05-10 12:27:57 +0200

    sezione 10 parte 2

--- screens/CreatePostScreen.js
+++ screens/CreatePostScreen.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 
 export default function Profile(props) {
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [description, setDescription] = useState('');
 
+  const onSumbitData = () => {
+
+  }
   return (
     <View style={styles.container}>
-        <Text>create post here</Text>
+        <Text style={styles.title}>Titolo del post</Text>
+        <TextInput style={styles.input} placeholder="scrivi il titolo" value={title} onChangeText={value => setTitle(value)}  />
+        <Text style={styles.title}>Immagine del post</Text>
+        <TextInput style={styles.input} placeholder="Scrivi l'URL " value={image} onChangeText={value => setImage(value)}  />
+        <Text style={styles.title}>Descrizione del post</Text>
+        <TextInput style={styles.input} placeholder="Scrivi la descrizione " value={description} onChangeText={value => setDescription(value)}  />
+        <Button title="Invia" style={{marginTop: 20}} onPress={onSumbitData} />
     </View>
   );
 }
@@ -15,5 +27,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center'
   },
+  title: {
+      fontSize: 20,
+      fontWeight: '500',
+      marginTop: 10
+  },
+  input: {
+      borderBottomWidth: 1,
+      width: '90%',
+      marginVertical: 10,
+      paddingLeft: 5,
+      paddingBottom: 5
+  }
 });
