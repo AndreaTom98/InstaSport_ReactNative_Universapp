4747a48e0bddb59df975c4001b2b5ba8ffd54443
AndreaTomasello <andrea.tomasello1498@gmail.com>
2020-05-10 13:27:26 +0200

    sezione 10 parte 5

--- screens/CreatePostScreen.js
+++ screens/CreatePostScreen.js
@@ -1,52 +1,79 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import axios from 'axios';
-
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  ScrollView
+} from "react-native";
+import {isIos} from '../utils/helper'
+import axios from "axios";
 
 export default function Profile(props) {
-  const [title, setTitle] = useState('');
-  const [image, setImage] = useState('');
-  const [description, setDescription] = useState('');
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
 
   const onSumbitData = async () => {
-    const url = 'https://instasport-66594.firebaseio.com/posts.json'
+    const url = "https://instasport-66594.firebaseio.com/posts.json";
     const data = await axios.post(url, {
-        title,
-        image,
-        description,
-    })
+      title,
+      image,
+      description
+    });
 
-    console.log(data)
-  }
+    console.log(data);
+  };
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView keyboardVerticalOffset={100} style={{flex:1}}>
+      <ScrollView contentContainerStyle={styles.container}>
         <Text style={styles.title}>Titolo del post</Text>
-        <TextInput style={styles.input} placeholder="scrivi il titolo" value={title} onChangeText={value => setTitle(value)}  />
+        <TextInput
+          style={styles.input}
+          placeholder="scrivi il titolo"
+          value={title}
+          onChangeText={value => setTitle(value)}
+        />
         <Text style={styles.title}>Immagine del post</Text>
-        <TextInput style={styles.input} placeholder="Scrivi l'URL " value={image} onChangeText={value => setImage(value)}  />
+        <TextInput
+          style={styles.input}
+          placeholder="Scrivi l'URL "
+          value={image}
+          onChangeText={value => setImage(value)}
+        />
         <Text style={styles.title}>Descrizione del post</Text>
-        <TextInput style={styles.input} placeholder="Scrivi la descrizione " value={description} onChangeText={value => setDescription(value)}  />
-        <Button title="Invia" style={{marginTop: 20}} onPress={onSumbitData} />
-    </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Scrivi l'URL "
+          value={image}
+          onChangeText={value => setImage(value)}
+        />
+        <Button
+          title="Invia"
+          style={{ marginTop: 20 }}
+          onPress={onSumbitData}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center'
+    alignItems: "center"
   },
   title: {
-      fontSize: 20,
-      fontWeight: '500',
-      marginTop: 10
+    fontSize: 20,
+    fontWeight: "500",
+    marginTop: 10
   },
   input: {
-      borderBottomWidth: 1,
-      width: '90%',
-      marginVertical: 10,
-      paddingLeft: 5,
-      paddingBottom: 5
+    borderBottomWidth: 1,
+    width: "90%",
+    marginVertical: 10,
+    paddingLeft: 5,
+    paddingBottom: 5
   }
 });
